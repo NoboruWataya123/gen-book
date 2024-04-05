bf82ad7061480196638424481cf64e0695e6a652
dkdkv <dulustan1996@gmail.com>
2024-04-05 15:55:31 +0900

    feat: russian text

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from ebooklib import epub
 import base64
 import requests
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
 stability_api_key = os.getenv('STABILITY_API_KEY')
@@ -16,7 +19,7 @@ def remove_first_line(test_string):
     return test_string
 
 
-def generate_text(prompt, model="claude-3-sonnet-20240229", max_tokens=2000, temperature=0.7):
+def generate_text(prompt, model="claude-3-opus-20240229", max_tokens=2000, temperature=0.7):
     headers = {
         "x-api-key": ANTHROPIC_API_KEY,
         "anthropic-version": "2023-06-01",
@@ -26,7 +29,7 @@ def generate_text(prompt, model="claude-3-sonnet-20240229", max_tokens=2000, tem
         "model": model,
         "max_tokens": max_tokens,
         "temperature": temperature,
-        "system": "You are a world-class author. Write the requested content with great skill and attention to detail.",
+        "system": "Вы - всемирно известный русскоязычный писатель. Ваша задача - создавать увлекательный, связный и грамматически правильный текст на русском языке с большим мастерством и вниманием к деталям.",
         "messages": [{"role": "user", "content": prompt}],
     }
     response = requests.post("https://api.anthropic.com/v1/messages", headers=headers, json=data)
@@ -37,13 +40,13 @@ def generate_text(prompt, model="claude-3-sonnet-20240229", max_tokens=2000, tem
 
 def generate_cover_prompt(plot):
     response = generate_text(
-        f"Plot: {plot}\n\n--\n\nDescribe the cover we should create, based on the plot. This should be two sentences long, maximum.")
+        f"Сюжет: {plot}\n\n--\n\nОпишите подробно, какой должна быть обложка книги, основываясь на приведенном сюжете. Обложка должна привлекать внимание и отражать ключевые моменты сюжета. Опишите цвета, композицию и основные элементы, которые должны присутствовать на обложке. Текст должен быть на русском языке и состоять из 3-4 предложений.")
     return response
 
 
 def generate_title(plot):
     response = generate_text(
-        f"Here is the plot for the book: {plot}\n\n--\n\nRespond with a great title for this book. Only respond with the title, nothing else is allowed.")
+        f"Вот сюжет книги: {plot}\n\n--\n\nПридумайте креативное, запоминающееся и интригующее название для этой книги на русском языке. Название должно отражать суть сюжета и привлекать потенциальных читателей. Отвечайте только названием книги, без дополнительного текста.")
     return remove_first_line(response)
 
 
@@ -92,49 +95,49 @@ def create_cover_image(plot):
 
 def generate_chapter_title(chapter_content):
     response = generate_text(
-        f"Chapter Content:\n\n{chapter_content}\n\n--\n\nGenerate a concise and engaging title for this chapter based on its content. Respond with the title only, nothing else.")
+        f"Содержание главы:\n\n{chapter_content}\n\n--\n\nСоздайте краткое, информативное и привлекательное название для этой главы на русском языке, основываясь на ее содержании. Название должно отражать ключевые события или тему главы. Отвечайте только названием главы, без дополнительного текста.")
     return remove_first_line(response)
 
 
-def sanitize_title(title):
+def sanitize_title(s_title):
     # Remove invalid characters for file names
-    sanitized_title = re.sub(r'[<>:"/\\|?*]', '', title)
+    san_title = re.sub(r'[<>:"/\\|?*]', '', s_title)
     # Replace spaces with underscores
-    sanitized_title = sanitized_title.replace(' ', '_')
-    return sanitized_title
+    san_title = san_title.replace(' ', '_')
+    return san_title
 
 
-def create_epub(title, author, chapters, cover_image_path='cover.png'):
-    book = epub.EpubBook()
+def create_epub(title_epub, author, chapters_epub, cover_image_path='cover.png'):
+    book_epub = epub.EpubBook()
     # Set metadata
-    book.set_identifier('id123456')
-    book.set_title(title)
-    book.set_language('ru')
-    book.add_author(author)
+    book_epub.set_identifier('id123456')
+    book_epub.set_title(title_epub)
+    book_epub.set_language('ru')
+    book_epub.add_author(author)
     # Add cover image
     cover_image_path = os.path.join(os.path.dirname(__file__), cover_image_path)
     with open(cover_image_path, 'rb') as cover_file:
         cover_image = cover_file.read()
-    book.set_cover('cover.png', cover_image)
+    book_epub.set_cover('cover.png', cover_image)
     # Create chapters and add them to the book
     epub_chapters = []
-    for i, chapter_content in enumerate(chapters):
+    for i, chapter_content in enumerate(chapters_epub):
         chapter_title = generate_chapter_title(chapter_content)
         chapter_file_name = f'chapter_{i + 1}.xhtml'
-        epub_chapter = epub.EpubHtml(title=chapter_title, file_name=chapter_file_name, lang='en')
+        epub_chapter = epub.EpubHtml(title=chapter_title, file_name=chapter_file_name, lang='ru')
         # Add paragraph breaks
         formatted_content = ''.join(
             f'<p>{paragraph.strip()}</p>' for paragraph in chapter_content.split('\n') if paragraph.strip())
         epub_chapter.content = f'<h1>{chapter_title}</h1>{formatted_content}'
-        book.add_item(epub_chapter)
+        book_epub.add_item(epub_chapter)
         epub_chapters.append(epub_chapter)
 
     # Define Table of Contents
-    book.toc = (epub_chapters)
+    book_epub.toc = epub_chapters
 
     # Add default NCX and Nav files
-    book.add_item(epub.EpubNcx())
-    book.add_item(epub.EpubNav())
+    book_epub.add_item(epub.EpubNcx())
+    book_epub.add_item(epub.EpubNav())
 
     # Define CSS style
     style = '''
@@ -151,36 +154,36 @@ def create_epub(title, author, chapters, cover_image_path='cover.png'):
 
     # Add CSS file
     nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
-    book.add_item(nav_css)
+    book_epub.add_item(nav_css)
 
     # Create spine
-    book.spine = ['nav'] + epub_chapters
+    book_epub.spine = ['nav'] + epub_chapters
 
     # Save the EPUB file
-    epub_file_path = os.path.join(os.path.dirname(__file__), f'{title}.epub')
-    epub.write_epub(epub_file_path, book)
+    epub_file_path = os.path.join(os.path.dirname(__file__), f'{title_epub}.epub')
+    epub.write_epub(epub_file_path, book_epub)
 
 
-def generate_book(writing_style, book_description, num_chapters):
-    print("Generating plot outline...")
-    plot_prompt = f"Create a detailed plot outline for a {num_chapters}-chapter book in the {writing_style} style, based on the following description:\n\n{book_description}\n\nEach chapter should be at least 10 pages long."
-    plot_outline = generate_text(plot_prompt)
-    print("Plot outline generated.")
+def generate_book(gen_writing_style, gen_book_description, gen_num_chapters):
+    print("Создание детальной схемы сюжета...")
+    plot_prompt = f"Создайте подробную схему сюжета для книги из {gen_num_chapters} глав в стиле {gen_writing_style}, основываясь на следующем описании:\n\n{gen_book_description}\n\nСхема должна включать завязку, развитие сюжета, кульминацию и развязку. Каждая глава должна быть не менее 10 страниц. Текст должен быть на русском языке."
+    gen_plot_outline = generate_text(plot_prompt)
+    print("Схема сюжета создана.")
 
-    chapters = []
-    for i in range(num_chapters):
-        print(f"Generating chapter {i + 1}...")
-        chapter_prompt = f"Previous Chapters:\n\n{' '.join(chapters)}\n\nWriting style: `{writing_style}`\n\nPlot Outline:\n\n{plot_outline}\n\nWrite chapter {i + 1} of the book, ensuring it follows the plot outline and builds upon the previous chapters. The chapter should be at least 256 paragraphs long... we're going for lengthy yet exciting chapters here."
+    gen_chapters = []
+    for i in range(gen_num_chapters):
+        print(f"Создание главы {i + 1}...")
+        chapter_prompt = f"Предыдущие главы:\n\n{' '.join(gen_chapters)}\n\nСтиль написания: `{gen_writing_style}`\n\nСхема сюжета:\n\n{gen_plot_outline}\n\nНапишите главу {i + 1} книги на русском языке, убедившись, что она соответствует схеме сюжета, опирается на предыдущие главы и продвигает историю вперед. Глава должна быть написана увлекательно, с интересными диалогами и описаниями. Используйте богатую и разнообразную лексику, чтобы полностью погрузить читателя в происходящее. Глава должна состоять не менее чем из 50 абзацев, мы стремимся к насыщенным и захватывающим главам."
         chapter = generate_text(chapter_prompt, max_tokens=4000)
-        chapters.append(remove_first_line(chapter))
-        print(f"Chapter {i + 1} generated.")
-        time.sleep(2)  # Add a short delay to avoid hitting rate limits
+        gen_chapters.append(remove_first_line(chapter))
+        print(f"Глава {i + 1} создана.")
+        time.sleep(65)  # Добавьте небольшую задержку, чтобы избежать превышения лимитов
 
     print("Compiling the book...")
-    book = "\n\n".join(chapters)
+    gen_book = "\n\n".join(gen_chapters)
     print("Book generated!")
 
-    return plot_outline, book, chapters
+    return gen_plot_outline, gen_book, gen_chapters
 
 
 # User input
